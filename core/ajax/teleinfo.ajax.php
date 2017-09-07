90c47a302f22290409aa858ec414f7e95437aeab
Halbout Michael <michaelhalbout@gmail.com>
2017-09-07 09:32:06 +0200

    Update teleinfo.ajax.php

--- core/ajax/teleinfo.ajax.php
+++ core/ajax/teleinfo.ajax.php
@@ -23,7 +23,133 @@ try {
 	if (!isConnect('admin')) {
 		throw new Exception('401 Unauthorized');
 	}
-	
+	if (init('action') == 'getTeleinfo') {
+		if (init('object_id') == '') {
+			$_GET['object_id'] = $_SESSION['user']->getOptions('defaultDashboardObject');
+		}
+		$object = object::byId(init('object_id'));
+		if (!is_object($object)) {
+			$object = object::rootObject();
+		}
+		if (!is_object($object)) {
+			throw new Exception('{{Aucun objet racine trouvé}}');
+		}
+		$return = array('object' => utils::o2a($object));
+
+		$date = array(
+			'start' => init('dateStart'),
+			'end' => init('dateEnd'),
+		);
+
+		if ($date['start'] == '') {
+			$date['start'] = date('Y-m-d', strtotime('-6 days' . date('Y-m-d')));
+		}
+		if ($date['end'] == '') {
+			$date['end'] = date('Y-m-d', strtotime('+1 days' . date('Y-m-d')));
+		}
+		$return['date'] = $date;
+		foreach ($object->getEqLogic() as $eqLogic) {
+			if ($eqLogic->getIsVisible() == '1' && $eqLogic->getEqType_name() == 'teleinfo') {
+				$return['eqLogics'][] = array('eqLogic' => utils::o2a($eqLogic), 'html' => $eqLogic->toHtml(init('version')));
+			}
+		}
+		ajax::success($return);
+	}
+
+	if (init('action') == 'getInformation') {
+		if(init('eqLogic_id') != null){
+			$eqLogic = eqLogic::byId(init('eqLogic_id'));
+			$return[$eqLogic->getId()] = utils::o2a($eqLogic);
+			$return[$eqLogic->getId()]['cmd'] = array();
+			foreach ($eqLogic->getCmd() as $cmd) {
+				$cmd_info = utils::o2a($cmd);
+				$cmd_info['value'] = $cmd->execCmd(null, 2);
+				$return[$eqLogic->getId()]['cmd'][] = $cmd_info;
+			}
+			ajax::success($return);			
+		}else{
+			$eqLogics = eqLogic::byType('teleinfo');
+			foreach ($eqLogics as $eqLogic) {
+				$return[$eqLogic->getId()] = utils::o2a($eqLogic);
+				$return[$eqLogic->getId()]['cmd'] = array();
+				foreach ($eqLogic->getCmd() as $cmd) {
+					$cmd_info = utils::o2a($cmd);
+					$cmd_info['value'] = $cmd->execCmd(null, 2);
+					$return[$eqLogic->getId()]['cmd'][] = $cmd_info;
+				}
+			}
+			ajax::success($return);
+		}
+	}
+	if (init('action') == 'getHealth') {
+		//$eqLogics = eqLogic::byType('teleinfo');
+		if(init('eqLogicID') != null){
+			$teleinfo = teleinfo::byLogicalId(init('eqLogicID'), 'teleinfo');
+			$health_cmd = $teleinfo->getCmd('info','health');
+			//$return[0] = $health_cmd;
+			$return = array('object' => utils::o2a($health_cmd));
+			$return["ADCO"] = init('eqLogicID');
+			ajax::success($return);
+		}
+		else{
+			$teleinfo = teleinfo::byType('teleinfo');
+			foreach ($teleinfo as $eqLogic) {
+				$health_cmd = $eqLogic->getCmd('info','health');
+				//$return[0] = $health_cmd;
+				$return = array('object' => utils::o2a($health_cmd));
+				$return["ADCO"] = $eqLogic->getLogicalId();
+				ajax::success($return);		
+			}
+		}
+
+		ajax::error("", "");
+	}
+	if (init('action') == 'getInfoDaemon') {
+
+		$return = array();
+		$_nbLines = 1000;
+		$replace = array(
+			'&gt;' => '>',
+			'&apos;' => '',
+		);
+		$page = array();
+
+		$path = realpath(dirname(__FILE__) . '/../../ressources/teleinfo.log');
+		//log::add('teleinfo', 'info', $path);
+		if (!file_exists($path)) {
+			$return['result'] = array('Deamon non lancé');
+			ajax::success($return);
+		}else{
+			$log = new SplFileObject($path);
+			if ($log) {
+				$log->seek(0); //Seek to the begening of lines
+				$linesRead = 0;
+				while ($log->valid() && $linesRead != $_nbLines) {
+					$line = trim($log->current()); //get current line
+					if ($line != '') {
+						array_unshift($page, $line);
+					}
+					$log->next(); //go to next line
+					$linesRead++;
+				}
+			}
+			//return $page;
+			$return['result'] = $page;
+			ajax::success($return);
+		}
+	}
+	if (init('action') == 'getHistory') {
+		$return = array();
+		$return = history::byCmdIdDatetime( init('id'), date('Y-m-d H:i:s'));
+		ajax::success($return);
+	}
+
+	if (init('action') == 'getCout') {
+		$return = array();
+		$return = history::byCmdIdDatetime( init('id'), date('Y-m-d H:i:s'));
+		ajax::success($return);
+	}
+
 
 	throw new Exception('Aucune methode correspondante');
 	/*     * *********Catch exeption*************** */
